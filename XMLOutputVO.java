8fa9de45381d121d63812409652c4d735a1333fc
jwnam0661 <jwnam0661@naver.com>
2019-03-01 15:16:20 +0900

    Create XMLOutputVO.java

---
+++ XMLOutputVO.java
@@ -0,0 +1,23 @@
+public class XMLOutputVO {
+	
+	String systemName = "";
+	
+	String categoryName = "";
+	
+	String btnName = "";
+	
+	String training = "";
+	
+	
+	public XMLOutputVO(String systemName, String categoryName, String btnName, String training) {
+		
+		this.systemName = systemName;
+		
+		this.categoryName = categoryName;
+		
+		this.btnName = btnName;
+		
+		this.training = training;
+		
+	}
+}
